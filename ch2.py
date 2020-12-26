65e4a35a4805ea6d75da4b7c35232590e856930d
vasanthramuk <vasanthvv1717@gmail.com>
2020-12-26 12:46:24 +0530

    Add files via upload

--- ch2.py
+++ ch2.py
@@ -1,3 +1,5 @@
+#Global variable
+
 def twoRobots(containerNo, query):
     robotAPos = robotBPos = 0
     instruction = []
@@ -13,11 +15,40 @@ def twoRobots(containerNo, query):
             raise ValueError
 
         instruction.append(temp)
+     
+#disctance covered
+    distanceCovered =[]
+    
+    if(robotAPos_temp == instruction[0][0]):
+        robotAPos = instruction[0][1]
+        
+    elif(robotBPos_temp == instruction[0][0]):
+        robotBPos = instruction[0][1]
 
+    
 
+    distanceCovered.append( abs(instruction[0][1] - instruction[0][0]))
+    #instruction.pop(0)
+    
+    if( robotAPos == instruction[1][0]):
+        robotAPos = instruction[1][1]
+        distanceCovered.append( abs(instruction[1][1] - instruction[1][0]))
+    else:   
+        robotBPos = instruction[1][1]
+        distanceCovered.append( abs(instruction[1][1] - instruction[1][0]))
 
-            
+    for i in range(2, len(instruction)): 
+        if ( abs(robotAPos -  instruction[i][0]) > abs(robotBPos -  instruction[i][0])):
+            robotBPos = instruction[i][1]
+            distanceCovered.append( abs(robotBPos -  instruction[i][0])+ abs(instruction[i][1] - instruction[i][0]))
+        else: 
+            robotAPos = instruction[i][1]
+            distanceCovered.append( abs(robotAPos -  instruction[i][0]+ abs(instruction[i][1] - instruction[i][0])))
+    
+    robotAPos_temp = robotAPos
+    robotBPos_temp = robotBPos
 
+    return sum(distanceCovered)
 try:
 
 ####################################################################################################
@@ -30,11 +61,13 @@ try:
         mn = input().split()
         containerNo = int(mn[0])
         query = int(mn[1])
-        if ((containerNo < 1 or containerNo > 1000) or (quiery < 1 or query > 1000)):
+        if ((containerNo < 1 or containerNo > 1000) or (query < 1 or query > 1000)):
             raise ValueError
 #insert towRobots() here
+        minDis = twoRobots()
+        print(minDis)
 
-
+        
         testCase -= 1
 
 ####################################################################################################
