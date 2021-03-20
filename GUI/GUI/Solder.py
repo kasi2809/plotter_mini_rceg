0ca4afbfb762d6e97f54880b7ce38fd037d878f2
girinath-ece <girinath01662@gmail.com>
2021-03-20 18:58:32 +0530

    app

---
+++ GUI/GUI/Solder.py
@@ -0,0 +1,226 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import Image,ImageTk
+import os, sys
+import traceback
+import time,cv2
+import cnc_pcb_v1_1
+import sqlite3
+
+i=0
+j=0
+
+'''
+try:
+    import cnc_pcb_v1_1
+
+except ImportError:
+    print('\n[ERROR] cnc_pcb_v1_1.py file is not present in the current directory.')
+    print('Your current directory is: ', os.getcwd())
+    print('Make sure cnc_pcb_v1_1.py is present in this current directory.\n')
+
+except Exception as e:
+    print('Your cnc_pcb_v1_1.py throwed an Exception. Kindly debug your code!\n')
+    traceback.print_exc(file=sys.stdout)
+'''
+
+window = Tk()
+window.iconbitmap('images/icon2.ico')
+window.geometry("750x250")
+window.resizable(width=False, height=False)
+window.title("SOLDER")
+
+fl = StringVar()
+cn = StringVar()
+var = StringVar()
+
+def exit1():
+    exit()
+
+def solder():
+    print('solder')
+    cnc_pcb_v1_1.send_data("3")
+
+def up():
+    print('up')
+    cnc_pcb_v1_1.send_data("5")
+
+def down():
+    print('down')
+    cnc_pcb_v1_1.send_data("6")
+
+def right():
+    print('right')
+    cnc_pcb_v1_1.send_data("7")
+
+def left():
+    print('left')
+    cnc_pcb_v1_1.send_data("8")
+
+def z_up():
+    print('z_up')
+    cnc_pcb_v1_1.send_data("1")
+
+def z_down():
+    print('z_down')
+    cnc_pcb_v1_1.send_data("2")
+
+
+def proced():
+    if (entry_1.get() == '' or myCombo.get() == "--Count--"):
+        messagebox.showinfo(title="Warning", message="Please fill all the entrys!")
+    else:
+        if(messagebox.askokcancel("Confirmation", "Want to continue?")):
+            proceed['state'] = DISABLED
+            brd=0
+            file_path=entry_1.get()
+            brd_count = int(myCombo.get())
+            print("File Path:",file_path)
+            print("Total Number of Boards:",brd_count)
+            points = cnc_pcb_v1_1.locate_coordinates(file_path)
+            print("Coordinates found")
+            cnc_pcb_v1_1.send_data("4"+","+str(brd_count)+","+str(len(points)))
+            sorted_points = cnc_pcb_v1_1.find_near_pts(points)
+            print("Coordinates sorted")
+            error = cnc_pcb_v1_1.calculate_error(sorted_points)
+            print("error calc")
+            steps = cnc_pcb_v1_1.calulating_steps(error)
+            print("calc steps")
+            while(brd<brd_count):
+                val = cnc_pcb_v1_1.send_data(steps)
+                brd+=1
+                print("No. of boards completed:",brd)
+            val=1
+            if val==1:
+                messagebox.showinfo(title="Done", message="Soldering is done!")
+                reset()
+
+def reset():
+    global i
+    i=0
+    myCombo.current(0)
+    entry_1.delete(0,'end')
+    if(myCombo.get() != "--Count--"):
+        num_brd.destroy()
+    proceed['state'] = NORMAL
+
+def abt():
+    messagebox.showinfo(title="About",message="This is a GUI Application done for the pcb soldering machine.\n Icon Credit:https://free-icon-rainbow.com/soldering-iron-icon-3/")
+
+def callibrate_win():
+    global i
+    if(i==1):
+        clb_window = Toplevel()
+        if (clb_window.winfo_exists()==1):
+            clb_window.destroy()
+    else:
+        i=1
+        clb_window = Toplevel()
+        clb_window.title("Calibrate")
+        clb_window.iconbitmap('images/icon2.ico')
+        clb_window.resizable(width=False, height=False)
+        clb_window.geometry("500x300")
+        clb_window.config(bg="white")
+        #Up Button
+        up_bn = PhotoImage(file='images/buttons/up.png')
+        up_btn = Button(clb_window,text="x",font=("arial",12,"bold"),image=up_bn,compound="center",relief="groove",command=up)
+        up_btn.place(x=100,y=30)
+        #Left Button
+        left_bn = PhotoImage(file='images/buttons/left.png')
+        left_btn = Button(clb_window, text="-y", font=("arial", 12, "bold"), image=left_bn, compound="center", relief="groove",command=left)
+        left_btn.place(x=30, y=100)
+        #Down Button
+        down_bn = PhotoImage(file='images/buttons/down.png')
+        down_btn = Button(clb_window, text="-x", font=("arial", 12, "bold"), image=down_bn, compound="center", relief="groove",command=down)
+        down_btn.place(x=100, y=170)
+        #Right Button
+        right_bn = PhotoImage(file='images/buttons/right.png')
+        right_btn = Button(clb_window, text="y", font=("arial", 12, "bold"), image=right_bn, compound="center", relief="groove",command=right)
+        right_btn.place(x=170, y=100)
+        #Z Up Button
+        z_up_bn = PhotoImage(file='images/buttons/z_up.png')
+        z_up_btn = Button(clb_window, text="z", font=("arial", 12, "bold"),fg="white", image=z_up_bn, compound="center",relief="groove",command=z_up)
+        z_up_btn.place(x=320, y=30)
+        #Z Down Button
+        z_down_bn = PhotoImage(file='images/buttons/z_down.png')
+        z_down_btn = Button(clb_window, text="-z",fg="white", font=("arial", 12, "bold"), image=z_down_bn, compound="center",relief="groove",command=z_down)
+        z_down_btn.place(x=320, y=170)
+        #start
+        solder_bn = PhotoImage(file='images/buttons/solder.png')
+        solder_btn = Button(clb_window,image=solder_bn, compound="center",relief="groove",command=solder)
+        solder_btn.place(x=320,y=100)
+
+        clb_window.mainloop()
+
+
+title = Label(window,text="Welcome to PCB Soldering",font=("georgia",16,"bold")).pack()
+
+img = Image.open('images/CEG_main_logo.png')
+photo = ImageTk.PhotoImage(img)
+logo = Label(image = photo)
+logo.place(x=525,y=10)
+
+menu = Menu(window)
+window.config(menu= menu)
+
+subm1 = Menu(menu)
+menu.add_cascade(label="File",menu=subm1)
+subm1.add_command(label="Exit",command=exit1)
+
+subm2 = Menu(menu)
+menu.add_cascade(label="Tools",menu=subm2)
+subm2.add_command(label="Callibrate",command=callibrate_win)
+
+subm3 = Menu(menu)
+menu.add_cascade(label="Options",menu=subm3)
+subm3.add_command(label="About",command=abt)
+
+file_loca = Label(window,text="Enter file location : ",font=("arial",12,"bold"))
+file_loca.place(x=10,y=40)
+
+entry_1 = Entry(window,textvar=fl,width='50')
+entry_1.insert(0,"")
+entry_1.place(x=160,y=44)
+
+num_brd = Label(window,text="Enter number of boards : ",font=("arial",12,"bold"))
+num_brd.place(x=10,y=80)
+i=1
+list_cnt=["--Count--"]
+while(i<=50):
+    list_cnt.append(i)
+    i+=1;
+
+myCombo = ttk.Combobox(window,value=list_cnt)
+myCombo.current(0)
+myCombo.place(x=210,y=84)
+
+# database
+
+data_base = sqlite3.connect('board_information.db')
+
+c = data_base.cursor()
+
+c.execute("""CREATE TABLE Board_Info 
+            (
+             File name text,
+             Board Count integer,
+             Number of Holes/Board integer,
+             Cost/Board float,
+             Total cost float
+             )                 """)
+
+data_base.commit()
+
+data_base.close()
+
+proceed = Button(window,text="Proceed",width=12,bg="green",fg="white",command = proced)
+proceed.place(x=50,y=140)
+
+rest = Button(window,text="Reset",width=12,bg="blue",fg="white",command = reset)
+rest.place(x=150,y=140)
+
+quit = Button(window,text="Exit",width=12,bg="red",fg="white",command = exit1)
+quit.place(x=250,y=140)
+
+window.mainloop()
--- Main Programs/cnc_pcb_v1.1.py
+++ Main Programs/cnc_pcb_v1.1.py
@@ -6,114 +6,113 @@
 #======== LIBRARIES IMPORTED =========
 import serial
 import xlrd
-import sys
 import math
 #========= GLOBAL VARIABLES ==========
-# try:
-#     ser=serial.Serial('com5',115200)
-# except:
-#     pass
-global entry_1,myCombo
 check_pts = {}
 mm_per_step_x=0.13
 mm_per_step_y=0.13
 points=[]
 redius = []
-board_count = 0
-total_board = 0
 
 #===== Locating the co-ordinates =====
 
-# try:
-#     file_path = entry_1.get()
-#     total_board = myCombo.get()
-# except:
-#     print("[ERROR]Enter valid details.")
-#     sys.exit()
-wb = xlrd.open_workbook("test_board.xls")
-sheet = wb.sheet_by_index(0)
+def locate_coordinates(file_path):
+    wb = xlrd.open_workbook(file_path)
+    sheet = wb.sheet_by_index(0)
 
-for i in range(len(sheet.col_values(2))):
-    coordinates = []
-    if sheet.cell_value(i,2) == 1:
-        rad = sheet.cell_value(i, 7)
-        redius.append(rad)
-        co_ord = sheet.cell_value(i,10)
-        co_ord=(co_ord[1:len(co_ord)-1]).split(')(')
-        for co_str in co_ord:
-            co_str=co_str.replace(' ',',')
-            co = co_str.split(',')
-            coordinates.append([float(co[0]),float(co[1])])
-            check_pts[rad] = coordinates
+    for i in range(len(sheet.col_values(2))):
+        coordinates = []
+        if sheet.cell_value(i,2) == 1:
+            rad = sheet.cell_value(i, 7)
+            redius.append(rad)
+            co_ord = sheet.cell_value(i,10)
+            co_ord=(co_ord[1:len(co_ord)-1]).split(')(')
+            for co_str in co_ord:
+                co_str=co_str.replace(' ',',')
+                co = co_str.split(',')
+                coordinates.append([float(co[0]),float(co[1])])
+                check_pts[rad] = coordinates
 
 
-print("Radius and points:",check_pts)
+    print("Radius and points:",check_pts)
 
-for r in redius:
-    for c in check_pts[r]:
-        c.append(r)
-        points.append(c)
-print("Total points to solder:",len(points))
-print("Points of PTH : ",points)
+    for r in redius:
+        for c in check_pts[r]:
+            c.append(r)
+            points.append(c)
+    print("Total points to solder:",len(points))
+    print("Points of PTH : ",points)
 
 #========= nearest points =========
-ref_pt = [0,0]
-sorted_points = []
-for j in range(0,len(points)):
-    min_dis = 0.00
-    for k in range(0,len(points)):
-        x = points[k][0]
-        y = points[k][1]
-        dis1 = pow(((x-ref_pt[0])**2+(y-ref_pt[1])**2),0.5)
-        if (dis1>min_dis and min_dis==0) or dis1<min_dis:
-            min_dis = dis1
-            t = k
-    ref_pt = points[t]
-    points.remove(ref_pt)
-    sorted_points.append(ref_pt)
-print("Final solder point co-ordinates:")
-print(sorted_points)
+def find_near_pts(pooints):
+    ref_pt = [0,0]
+    sorted_points = []
+    for j in range(0,len(pooints)):
+        min_dis = 0.00
+        for k in range(0,len(pooints)):
+            x = pooints[k][0]
+            y = pooints[k][1]
+            dis1 = pow(((x-ref_pt[0])**2+(y-ref_pt[1])**2),0.5)
+            if (dis1>min_dis and min_dis==0) or dis1<min_dis:
+                min_dis = dis1
+                t = k
+        ref_pt = pooints[t]
+        pooints.remove(ref_pt)
+        sorted_points.append(ref_pt)
+    print("Final solder point co-ordinates:")
+    print(sorted_points)
+    return sorted_points
 
 #===== Printing difference b/w 2 points =====
-error=[]
-for i in range(len(sorted_points)-1):
-    p1x=sorted_points[i][0]
-    p1y=sorted_points[i][1]
-    p2x=sorted_points[i+1][0]
-    p2y=sorted_points[i+1][1]
-    diff_x=p2x-p1x
-    diff_x= round(diff_x, 2)
-    diff_y=p2y-p1y
-    diff_y = round(diff_y, 2)
-    error.append([diff_x,diff_y,sorted_points[i][2]])
-error.insert(0,sorted_points[0])
-print("Difference:")
-print(error)
-
+def calculate_error(sort_points):
+    error=[]
+    for i in range(len(sort_points)-1):
+        p1x=sort_points[i][0]
+        p1y=sort_points[i][1]
+        p2x=sort_points[i+1][0]
+        p2y=sort_points[i+1][1]
+        diff_x=p2x-p1x
+        diff_x= round(diff_x, 2)
+        diff_y=p2y-p1y
+        diff_y = round(diff_y, 2)
+        error.append([diff_x,diff_y,sort_points[i][2]])
+    error.insert(0,sort_points[0])
+    print("Difference:")
+    print(error)
+    return error
 #===== Calculating steps needed to move =====
-steps=[]
-for s in range(len(error)):
-    steps_x=int(error[s][0]/mm_per_step_x)
-    steps_y=int(error[s][1]/mm_per_step_y)
-    steps.append([steps_x,steps_y,int(error[s][2])+1])
-    steps.append([steps_x, steps_y, math.ceil(error[s][2])])
+def calulating_steps(err):
+    stps=[]
+    for s in range(len(err)):
+        steps_x=int(err[s][0]/mm_per_step_x)
+        steps_y=int(err[s][1]/mm_per_step_y)
+        stps.append([steps_x,steps_y,int(err[s][2])+1])
+        stps.append([steps_x, steps_y, math.ceil(err[s][2])])
 
-print("Steps needed:")
-print(steps)
+    print("Steps needed:")
+    print(stps)
+    return stps
 
 #===== Sending data to Microcontroller ======
-# try:
-#     while(board_count<total_board):
-#         index=0
-#         while True:
-#             if str(ser.readline())=="b\'NEXT\\r\\n\'":
-#                 value = str(steps[index][0])+","+str(steps[index][1])+str(steps[index][2])+","+"\n"
-#                 ser.write(value.encode('utf-8'))
-#                 index+=1
-#             if index==len(steps):
-#                 break
-#         board_count += 1
-#         print("Number of boards completed:", board_count)
-#     sys.exit()
-# except:
-#     print("[ERROR]Sending data to microcontroller.Please restart machine.")
+def send_data(tot_brd,stps):
+    board_count = 0
+    tot_brd = 0
+    try:
+        ser=serial.Serial('com5',115200)
+    except:
+        print("[ERROR]Connection Not Established.")
+    try:
+        while(board_count<tot_brd):
+            index=0
+            while True:
+                if str(ser.readline())=="b\'NEXT\\r\\n\'":
+                    value = str(stps[index][0])+","+str(stps[index][1])+str(stps[index][2])+","+"\n"
+                    ser.write(value.encode('utf-8'))
+                    index+=1
+                if index==len(stps):
+                    break
+            board_count += 1
+            print("Number of boards completed:", board_count)
+        return 1
+    except:
+        print("[ERROR]Sending data to microcontroller.Please restart machine.")
